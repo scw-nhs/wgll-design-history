1cae18ca08e3b56be59c31f5eed9d87de22bf9ed
Brierley Sam (NHS SCWCSU) <Sam.Brierley@SCWCSU.NHS.UK>
2025-10-17 17:52:42 +0100

    wgll-upload

---
+++ eleventy.config.js
@@ -0,0 +1,25 @@
+import process from 'node:process'
+
+import { nhsukEleventyPlugin } from '@x-govuk/nhsuk-eleventy-plugin'
+
+export default function(eleventyConfig) {
+  // Register the plugin
+  eleventyConfig.addPlugin(nhsukEleventyPlugin, {
+    header: {
+      service: {
+        text: 'WGLL design history'
+      },
+		}
+	})
+
+  return {
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+		pathPrefix: process.env.GITHUB_ACTIONS ? '/wgll-design-history/' : '/',
+    dir: {
+      // The folder where all your content will live:
+      input: 'docs',
+    }
+  }
+};
